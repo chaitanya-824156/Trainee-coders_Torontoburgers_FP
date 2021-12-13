dea9acf4302d9a01675b730636f2ba2a1a65780b
chaitanya-824156 <c0824156@mylambton.ca>
2021-12-13 11:48:45 -0500

    Added images

--- data.js
+++ data.js
@@ -4,7 +4,7 @@ const app_data = {
 			id: 101,
 			name: 'Bacon Delux',
 			cal: '930 - 080',
-			image: './images/group.jpg',
+			image: './images/bacon deluxe.jpg',
 			price: 300,
 			description: 'Fresh Canadian beef patty with fries and drink',
 			category: 'combos'
@@ -12,7 +12,7 @@ const app_data = {
 		'102': {
 			id: 102,
 			name: 'Chicken',
-			image: './images/group.jpg',
+			image: './images/chicken sandwich.jpg',
 			price: 300,
 			cal: '930 - 080',
 			description: 'Fresh Canadian beef patty with fries and drink',
@@ -20,30 +20,30 @@ const app_data = {
 		},
 		'103': {
 			id: 103,
-			name: 'Bacon Delux',
-			image: './images/group.jpg',
-			cal: '930 - 080',
-			description: 'Fresh Canadian beef patty with fries and drink',
-			price: 300,
-			category: 'combos'
+			name: 'Five piece Chicken nuggets',
+			image: './images/5 pc chicken nuggets.jpg',
+			cal: '200 - 250',
+			description: 'Chicken breaded crispy served with dips',
+			price: 30,
+			category: 'chicken and wraps'
 		},
 		'104': {
 			id: 104,
-			name: 'Bacon Delux',
-			image: './images/group.jpg',
-			price: 300,
-			cal: '930 - 080',
-			description: 'Fresh Canadian beef patty with fries and drink',
-			category: 'combos'
+			name: 'Chicken wrap',
+			image: './images/chicken wrap.jpg',
+			price: 20,
+			cal: '450 - 470',
+			description: 'juicy crispy chicken breast',
+			category: 'chicken and wraps'
 		},
 		'105': {
 			id: 105,
-			name: 'Bacon Delux',
-			image: './images/group.jpg',
-			price: 300,
-			cal: '930 - 080',
-			description: 'Fresh Canadian beef patty with fries and drink',
-			category: 'combos'
+			name: 'Diet coke',
+			image: './images/diet coke.jpg',
+			price: 10,
+			cal: '100 - 140',
+			description: 'cool drink',
+			category: 'Beverages'
 		}
 	}
 }
